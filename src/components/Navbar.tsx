89f97ce430a9516d24aed452d91536a496c7004c
Oscar Machado <plus44b@hotmail.com>
2024-09-12 20:23:34 -0400

    Link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,6 +6,7 @@ import {
   FaSquareXTwitter,
 } from "react-icons/fa6";
 import Logo from "../assets/KK.png";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -14,6 +15,12 @@ const Navbar = () => {
         <img src={Logo} alt="KixKira Logo" className="mx-2 w-14" />
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <Link
+          to="/landing-page"
+          className="bg-[#FF0000] hover:bg-[#ff00005b] text-white font-bold py-2 px-4 rounded"
+        >
+          Planes
+        </Link>
         <a
           href="https://www.facebook.com/profile.php?id=61564566646979/"
           target="_blank"
